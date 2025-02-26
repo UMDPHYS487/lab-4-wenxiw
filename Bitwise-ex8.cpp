679a07f419513af609aa128972253dba750a5a85
Wenxi Wu <87095574+wenxiw@users.noreply.github.com>
2025-02-26 20:48:11 +0000

    bit

--- Bitwise-ex8.cpp
+++ Bitwise-ex8.cpp
@@ -1,1 +1,15 @@
 // Your code here
+#include <iostream>
+#include <bitset>
+
+int main(){
+    std:: bitset<8> b{0b01110010};
+        std::cout << b << "(initial value)\n";
+    
+        for(; b.any(); b>>=1){
+            while (!b.test(0))
+                b >>= 1;
+            std::cout << b << '\n';
+        }
+        std::cout << b << "(final value)\n";
+}
