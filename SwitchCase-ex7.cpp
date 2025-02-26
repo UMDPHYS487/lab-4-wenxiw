b045558855fa2ff2da724c94fb19d50738fd0753
Wenxi Wu <87095574+wenxiw@users.noreply.github.com>
2025-02-26 20:25:34 +0000

    case

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,34 @@
 // add your code here
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<< "Please enter an interger value here: ";
+    cin >> n;
+    
+    int i; 
+    if(n<10 && n%2==0){
+        i = 0;
+    }
+    else if(n%2==1){
+        i = 1;
+    }
+    else 
+        i = 2;
+
+    switch(i){
+    case 0:
+        cout << n*n << '\n';
+        break;
+    
+    case 1:
+        cout<< n/2.0 << '\n';
+        break;
+
+    case 2:
+        cout <<"hahaha" << '\n';
+        break;
+    
+    }
+}
