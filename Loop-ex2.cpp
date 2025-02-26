b00f6128c02544011d45cb672fa80f86ea1562df
Wenxi Wu <87095574+wenxiw@users.noreply.github.com>
2025-02-26 18:42:24 +0000

    10 times!

--- Loop-ex2.cpp
+++ Loop-ex2.cpp
@@ -1,1 +1,13 @@
 // add your code here
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 0;
+    while (n<10) {
+        cout << "This is running ";
+        cout << n <<" time! \n";
+        n++;
+
+    }
+}
